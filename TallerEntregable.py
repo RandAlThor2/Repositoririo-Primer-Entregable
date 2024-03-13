f1ca7c200bd85968fe53db15fe95b5971a6c12da
karmaly <karol.alvarez9@gmail.com>
2024-03-13 08:58:32 -0500

    Avance en funciones para la clase Sistema

--- TallerEntregable.py
+++ TallerEntregable.py
@@ -170,8 +170,84 @@ class Paciente:
         self.__cedula = c
     def set_Genero(self,g):
         self.__genero = g
-    def set_listaImplantesPac(self, li):
-        self.__listaimplantes = li
+
+    def agregarimplante(self, id, ob):
+        self.get_listaImplantesPac()[id] = ob
+        return print("Implante ingresado")
 
 class Sistema:
-     
+    def __init__(self):
+        self.__pacientes = {}
+        self.__implantes = {
+            "ImplanteRodilla" : {},
+            "ImplanteCadera" : {},
+            "ImplanteDental" : {},
+            "StentCoronario" : {},
+            "Marcapasos" : {}
+        }
+           
+    def get_implantes(self):
+        return self.__implantes
+    def get_pacientes(self):
+        return self.__pacientes
+    
+    def verificarExistePac(self, id):
+        if id in self.__pacientes:
+            return True
+        else:
+            return False
+            
+    def verificarExisteImp(self, tipoimp,  id):
+        if tipoimp == "Implante de Rodilla":
+            if id in self.__implantes["ImplanteRodilla"]:
+                return True
+        elif tipoimp == "Implante de Cadera":
+            if id in self.__implantes["ImplanteCadera"]:
+                return True
+        elif tipoimp == "Implante Dental":
+            if id in self.__implantes["ImplanteDental"]:
+                return True
+        elif tipoimp == "Stent Coronario":
+            if id in self.__implantes["StentCoronario"]:
+                return True
+        elif tipoimp == "Marcapasos":
+            if id in self.__implantes["Marcapasos"]:
+                return True
+        else:
+            return False
+            
+    def agregarPac(self, id, ob):
+        a = self.verificarExiste()
+        if a:
+            self.get_pacientes()[id] = ob
+            return False
+        else: 
+            return True
+        
+    def agregarImp(self, id, tipoimp, ob):
+        a = self.verificarExisteImp()
+        if a:
+            return print(f"El implante tipo {tipoimp} con {id} ya se halla en la base de datos")
+        else:
+            if tipoimp == "Implante de Rodilla":
+                self.__implantes["ImplanteRodilla"][id] = ob
+                return print("Implante de Rodilla ingresado")
+            elif tipoimp == "Implante de Cadera":
+                self.__implantes["ImplanteCadera"][id] = ob
+                return print("Implante de Cadera ingresado")
+            elif tipoimp == "Implante Dental":
+                self.__implantes["ImplanteDental"][id] = ob
+                return print("Implante Dental ingresado")
+            elif tipoimp == "Stent Coronario":
+                self.__implantes["StentCoronario"][id] = ob
+                return print("StentCoronario ingresado")
+            elif tipoimp == "Marcapasos":
+                self.__implantes["Marcapasos"][id] = ob
+                return print("Marcapasos ingresado")
+            
+    def editarInfoImplante(self, tipo, id, nuevo):
+            self.get_implantes()[id].a = nuevo
+    
+    
+
+    
