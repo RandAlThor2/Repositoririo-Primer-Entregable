645016d56bcce71ddc5d1a7c34e1d34a0c4f4373
RandAlThor2 <montoya.fernandez202@gmail.com>
2024-03-13 18:34:20 -0500

    Menú opción 3 terminada.

--- TallerEntregable.py
+++ TallerEntregable.py
@@ -570,5 +570,15 @@ def main():
                 break
         
         elif menu==3:
-            id
+            while True:
+                pac=Paciente()    
+                id=int(input("Ingrese la cedula del paciente: "))
+                if servicio_implantes.verificarExistePac(id):
+                    print("El paciente ya se encuentra registrado")
+                    break
+                else:
+                    pac.set_Nombre=input("Ingrese el nombre del paciente: ")
+                    pac.set_Cedula=id
+                    pac.set_Genero=input("Ingrese el genero del paciente: ")
+                    servicio_implantes.agregarPac(id,pac)
 main()
