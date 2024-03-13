fada79bfae949ab46797e1bd83066b732240e893
karmaly <karol.alvarez9@gmail.com>
2024-03-13 00:52:43 -0500

    Avances en estructura de las clases hijas de los implantes

--- TallerEntregable.py
+++ TallerEntregable.py
@@ -24,7 +24,12 @@ class Implante:
        self.__mantenimiento = man
 
 class ImplanteRodilla:
-    def __init__(self):
+    def __init__(self, medResponsable, estado, fechaRevision, mantenimiento):
+        super().__init__(medResponsable, estado, fechaRevision, mantenimiento)
+        self.set_medResponsable(medResponsable)
+        self.set_estado(estado)
+        self.set_fechaRevision(fechaRevision)
+        self.set_mantenimiento(mantenimiento)
         self.__material = ""
         self.__tipoFijacion = ""
         self.__tamaño = float
@@ -42,10 +47,14 @@ class ImplanteRodilla:
         self.__tipoFijacion = tf
     def set_tamaño(self, tam):
         self.__tamaño = tam
-
 
 class ImplanteCadera:
-    def __init__(self):
+    def __init__(self, medResponsable, estado, fechaRevision, mantenimiento):
+        super().__init__(medResponsable, estado, fechaRevision, mantenimiento)
+        self.set_medResponsable(medResponsable)
+        self.set_estado(estado)
+        self.set_fechaRevision(fechaRevision)
+        self.set_mantenimiento(mantenimiento)
         self.__material = ""
         self.__tipoFijacion = ""
         self.__tamaño = float
@@ -63,3 +72,55 @@ class ImplanteCadera:
         self.__tipoFijacion = tf
     def set_tamaño(self, tam):
         self.__tamaño = tam
+
+class ImplanteDental:
+    def __init__(self, medResponsable, estado, fechaRevision, mantenimiento):
+        super().__init__(medResponsable, estado, fechaRevision, mantenimiento)
+        self.set_medResponsable(medResponsable)
+        self.set_estado(estado)
+        self.set_fechaRevision(fechaRevision)
+        self.set_mantenimiento(mantenimiento)
+        self.__material = ""
+        self.__forma = ""
+        self.__sistFijacion = ""
+
+    def get_material(self):
+        return self.__material
+    def get_forma(self):
+        return self.__forma
+    def get_sistFijacion(self):
+        return self.__sistFijacion
+    
+    def set_material(self, mat):
+        self.__material = mat
+    def set_forma(self, f):
+        self.__forma = fs
+    def set_sistFijacion(self, sf):
+        self.__sistFijacion = sf
+
+class StentCoronario:
+    def __init__(self, medResponsable, estado, fechaRevision, mantenimiento):
+        super().__init__(medResponsable, estado, fechaRevision, mantenimiento)
+        self.set_medResponsable(medResponsable)
+        self.set_estado(estado)
+        self.set_fechaRevision(fechaRevision)
+        self.set_mantenimiento(mantenimiento)
+        self.__material = ""
+        self.longitud = float
+        self.__diametro = float
+
+    def get_material(self):
+        return self.__material
+    def get_longitud(self):
+        return self.__longitud
+    def get_diametro(self):
+        return self.__diametro
+    
+    def set_material(self, mat):
+        self.__material = mat
+    def set_longitud(self, l):
+        self.__longitud = l
+    def set_diametro(self, d):
+        self.__diametro = d
+
+        
